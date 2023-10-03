ef8fa9fca14ca5a54d6f9b108172df2537bf4be2
Henry Nguyen <5065089+nguyengg@users.noreply.github.com>
2023-10-03 09:55:05 -0700

    Add utility to parse S3 URI with bucket owner

---
+++ s3uri/s3uri.go
@@ -0,0 +1,39 @@
+package internal
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// S3URIWithOwner contains required Bucket, Key, and ExpectedBucketOwner fields.
+type S3URIWithOwner struct {
+	Bucket              string
+	Key                 string
+	ExpectedBucketOwner string
+}
+
+var s3UriWithOwnerPattern = regexp.MustCompile(`s3://([a-z0-9][a-z0-9.-]+?)\[(\d+)](/(.*))?`)
+
+// ParseS3URIWithOwner parses a URL in expected format s3://bucket[owner]/key.
+//
+// Only the bucket name and expected bucket owner is required. The key can be empty, or can be a prefix that possibly
+// ends in "/".
+func ParseS3URIWithOwner(rawURL string) (value S3URIWithOwner, err error) {
+	if !strings.HasPrefix(rawURL, "s3://") {
+		err = fmt.Errorf("URL does not start with s3://")
+		return
+	}
+
+	m := s3UriWithOwnerPattern.FindStringSubmatch(rawURL)
+	if len(m) != 5 {
+		err = fmt.Errorf("URL is not in format s3://bucket[owner]/key")
+		return
+	}
+
+	value.Bucket = m[1]
+	value.ExpectedBucketOwner = m[2]
+	value.Key = m[4]
+
+	return
+}
---
+++ s3uri/s3uri_test.go
@@ -0,0 +1,78 @@
+package internal
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseS3URIWithOwner(t *testing.T) {
+	type args struct {
+		rawURL string
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantValue S3URIWithOwner
+		wantErr   bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "S3 URI to a file",
+			args: args{rawURL: "s3://my-bucket[1234]/path/to/file.json"},
+			wantValue: S3URIWithOwner{
+				Bucket:              "my-bucket",
+				Key:                 "path/to/file.json",
+				ExpectedBucketOwner: "1234",
+			},
+		},
+		{
+			name: "S3 prefix path",
+			args: args{rawURL: "s3://my-bucket[1234]/path/to/"},
+			wantValue: S3URIWithOwner{
+				Bucket:              "my-bucket",
+				Key:                 "path/to/",
+				ExpectedBucketOwner: "1234",
+			},
+		},
+		{
+			name: "optional S3 key",
+			args: args{rawURL: "s3://my-bucket[1234]/"},
+			wantValue: S3URIWithOwner{
+				Bucket:              "my-bucket",
+				Key:                 "",
+				ExpectedBucketOwner: "1234",
+			},
+		},
+		{
+			name: "optional S3 key without trailing slash",
+			args: args{rawURL: "s3://my-bucket[1234]"},
+			wantValue: S3URIWithOwner{
+				Bucket:              "my-bucket",
+				Key:                 "",
+				ExpectedBucketOwner: "1234",
+			},
+		},
+		{
+			name:    "not an S3 URI",
+			args:    args{rawURL: "https://smile.amazon.com"},
+			wantErr: true,
+		},
+		{
+			name:    "missing bucket owner",
+			args:    args{rawURL: "s3://my-bucket/path/to/file.json"},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotValue, err := ParseS3URIWithOwner(tt.args.rawURL)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseS3URIWithOwner() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotValue, tt.wantValue) {
+				t.Errorf("ParseS3URIWithOwner() gotValue = %v, want %v", gotValue, tt.wantValue)
+			}
+		})
+	}
+}
