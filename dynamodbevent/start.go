920090317344169f72bbcb0ac3edfb2908ddae0e
Henry Nguyen <5065089+nguyengg@users.noreply.github.com>
2023-10-03 14:20:42 -0700

    Add a wrapper for DynamoDB events that report batch item failure

--- dynamodbevent/start.go
+++ dynamodbevent/start.go
@@ -12,8 +12,11 @@ import (
 	"log"
 )
 
-// Handler for DynamoDB events.
+// Handler for DynamoDB events that doesn't return a response, and as a result cannot report batch item failure.
 type Handler func(ctx context.Context, request events.DynamoDBEvent) error
+
+// HandlerWithResponse for DynamoDB events and returns a response to report batch item failure.
+type HandlerWithResponse func(ctx context.Context, request events.DynamoDBEvent) (events.DynamoDBEventResponse, error)
 
 // Start starts the Lambda runtime loop with the specified Handler.
 func Start(handler Handler, options ...start.Option) {
@@ -57,6 +60,54 @@ func Start(handler Handler, options ...start.Option) {
 		}
 
 		err = handler(ctx, request)
+		panicked = false
+		return
+	})
+}
+
+// StartHandlerWithResponse starts the Lambda runtime loop with the specified HandlerWithResponse.
+func StartHandlerWithResponse(handler HandlerWithResponse, options ...start.Option) {
+	opts := start.New(options)
+
+	lambda.Start(func(ctx context.Context, request events.DynamoDBEvent) (response events.DynamoDBEventResponse, err error) {
+		m := metrics.NewSimpleMetricsContext(
+			opts.LoggerProvider(ctx).WithContext(ctx),
+			"",
+			0)
+		ctx = m.WithContext(ctx)
+
+		if !opts.DisableSetUpGlobalLogger {
+			defer logsupport.SetUpGlobalLogger(ctx)()
+		}
+
+		if !opts.DisableRequestDebugLogging && configsupport.IsDebug() {
+			data, err := json.Marshal(request)
+			if err != nil {
+				log.Printf("ERROR marshal request: %v\n", err)
+			} else {
+				log.Printf("INFO request: %s\n", data)
+			}
+		}
+
+		panicked := true
+
+		if !opts.DisableMetricsLogging {
+			m.AddCount("recordCount", int64(len(request.Records)))
+			m.AddCount("batchItemFailureCount", int64(len(response.BatchItemFailures)))
+
+			defer func() {
+				if panicked {
+					m.Panicked()
+				}
+				if err != nil {
+					m.Faulted()
+				}
+
+				m.Log()
+			}()
+		}
+
+		response, err = handler(ctx, request)
 		panicked = false
 		return
 	})
