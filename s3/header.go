aaabecd55f3a673007b23f1caf0633546f9d0f00
Henry Nguyen <5065089+nguyengg@users.noreply.github.com>
2023-10-03 17:33:32 -0700

    Add function to parse headers from S3 Get/Head

--- s3/header.go
+++ s3/header.go
@@ -3,6 +3,7 @@ package s3
 import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -34,6 +35,65 @@ func AddToHeadObject(input *s3.HeadObjectInput, header http.Header) *s3.HeadObje
 	input.IfUnmodifiedSince = getIfUnmodifiedSince(header)
 	input.Range = getRange(header)
 	return input
+}
+
+// HeadersFromGetObjectOutput parses response headers from the [s3.GetObjectOutput] and passes it to the callback.
+func HeadersFromGetObjectOutput(output *s3.GetObjectOutput, cb func(k, v string)) {
+	if output.ContentDisposition != nil {
+		cb("Content-Disposition", *output.ContentDisposition)
+	}
+	if output.ContentEncoding != nil {
+		cb("Content-Encoding", *output.ContentEncoding)
+	}
+	if output.ContentLanguage != nil {
+		cb("Content-Language", *output.ContentLanguage)
+	}
+	if output.ContentLength != 0 {
+		cb("Content-Length", strconv.FormatInt(output.ContentLength, 10))
+	}
+	if output.ContentRange != nil {
+		cb("Content-Range", *output.ContentRange)
+	}
+	if output.ContentType != nil {
+		cb("Content-Type", *output.ContentType)
+	}
+	if output.ETag != nil {
+		cb("ETag", *output.ETag)
+	}
+	if output.Expires != nil {
+		cb("Expires", output.Expires.Format(http.TimeFormat))
+	}
+	if output.LastModified != nil {
+		cb("Last-Modified", output.Expires.Format(http.TimeFormat))
+	}
+}
+
+// HeadersFromHeadObjectOutput parses response headers from the [s3.HeadObjectOutput] and passes it to the callback.
+func HeadersFromHeadObjectOutput(output *s3.HeadObjectOutput, cb func(k, v string)) {
+	if output.ContentDisposition != nil {
+		cb("Content-Disposition", *output.ContentDisposition)
+	}
+	if output.ContentEncoding != nil {
+		cb("Content-Encoding", *output.ContentEncoding)
+	}
+	if output.ContentLanguage != nil {
+		cb("Content-Language", *output.ContentLanguage)
+	}
+	if output.ContentLength != 0 {
+		cb("Content-Length", strconv.FormatInt(output.ContentLength, 10))
+	}
+	if output.ContentType != nil {
+		cb("Content-Type", *output.ContentType)
+	}
+	if output.ETag != nil {
+		cb("ETag", *output.ETag)
+	}
+	if output.Expires != nil {
+		cb("Expires", output.Expires.Format(http.TimeFormat))
+	}
+	if output.LastModified != nil {
+		cb("Last-Modified", output.Expires.Format(http.TimeFormat))
+	}
 }
 
 func getIfMatch(header http.Header) *string {
